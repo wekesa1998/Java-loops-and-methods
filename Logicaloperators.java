32e9337f5e6176d4a2c1311b7b82115cb8b6dd21
wekesa1998 <duncanwekesa2015@gmail.com>
2017-03-13 10:25:48 +0300

    using math class and logical operators AND ,OR

---
+++ Logicaloperators.java
@@ -0,0 +1,15 @@
+
+public class Logicaloperators {
+	public static void main(String[] args){
+		int y=6,x=8;
+		//using logical operator and to test validity of the expression. 
+		//which executes only if both are true.
+		//you can use || in place of && to test for or which executes if any of the expression is true
+		if(y<10 && x<10){
+		System.out.println("enter the value");
+		}else{
+			System.out.println("you can't enter");
+		}
+	}
+
+}
---
+++ Mathclass.java
@@ -0,0 +1,11 @@
+
+public class Mathclass {
+public static void main(String[] args){
+	System.out.println(Math.abs(-67.90));
+	System.out.println(Math.max(456, 342));
+	System.out.println(Math.min(34, 67));
+	System.out.println(Math.sqrt(49));
+	System.out.println(Math.ceil(78.4));
+	System.out.println(Math.pow(3,2));
+}
+}
